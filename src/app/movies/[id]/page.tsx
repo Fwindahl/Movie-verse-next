452fe397671e36c80546402e0cede35e759d823b
Fwindahl <filipwindahl@hotmail.com>
2024-04-25 19:56:22 +0200

    mobile responsive

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,3 +1,4 @@
+"use server";
 import Link from "next/link";
 import React from "react";
 import { getMovieDetails, getSimilarMovies } from "../../../../utils/requests";
@@ -17,46 +18,57 @@ const MovieDetailsPage: React.FC<MovieDetailsPageProps> = async ({
   const similarMovies = await getSimilarMovies(params.id);
   console.log(similarMovies);
   return (
-    <main className="w-full   flex h-full justify-between gap-5">
-      <section className="flex-col max-w-3xl pl-32 flex items-start justify-center gap-5 w-full">
-        <div className="flex flex-col gap-5">
-          <Image
-            loading="lazy"
-            width={50}
-            height={100}
-            alt="Movie Poster"
-            src={IMAGE_BASE_URL + movieDetails.poster_path}
-          />
-
-          <div>
-            <h1 className="text-5xl">{movieDetails.title}</h1>
-            <h5>{movieDetails.tagline}</h5>
-          </div>
-          <p>
-            {movieDetails.release_date}{" "}
-            <span className="p-1 rounded-md text-black bg-[#00FF99]">
-              {movieDetails.status}
-            </span>
-          </p>
-
-          <p className="flex  gap-2">
-            {movieDetails.genres.map((genre: any) => {
-              return (
-                <span className="p-1 border border-[#00FF99] rounded-md hover:bg-[#00FF99] hover:text-black transition-all duration-200">
-                  {genre.name}
-                </span>
-              );
-            })}
-          </p>
-        </div>
-        <p>{movieDetails.overview}</p>
+    <main className="w-full max-w-screen-md:flex-wrap-reverse relative  flex h-full justify-between gap-5">
+      <section className="py-10 w-full pl-10  ">
         <Button asChild variant="outline">
-          <Link href={"/"}>Back to home</Link>
+          <Link className="back-btn" href={"/"}>
+            Back to home
+          </Link>
         </Button>
+        <section className="flex-col max-w-full flex-wrap mobile-pad  flex items-start justify-start gap-5 w-full">
+          <div className="flex gap-5 justify-start   items-start w-full max-w-full">
+            <div className=" w-[60%] flex flex-col gap-3">
+              <div>
+                <h1 className="rwd-h1">{movieDetails.title}</h1>
+                <h5>{movieDetails.tagline}</h5>
+              </div>
+              <p className="flex max-detail-container gap-3 items-center flex-wrap">
+                {movieDetails.release_date}{" "}
+                <span className="p-1 rounded-md text-black bg-[#00FF99]">
+                  {movieDetails.status}
+                </span>
+              </p>
+
+              <p className=" max-detail-container flex flex-wrap  gap-2">
+                {movieDetails.genres.map((genre: any, index: number) => {
+                  return (
+                    <span
+                      key={index}
+                      className="p-1 text-center border border-[#00FF99] rounded-md hover:bg-[#00FF99] hover:text-black transition-all duration-200"
+                    >
+                      {genre.name}
+                    </span>
+                  );
+                })}
+              </p>
+            </div>
+            <div>
+              <Image
+                className="max-w-full poster-shadow w-full"
+                loading="lazy"
+                width={100}
+                height={100}
+                alt="Movie Poster"
+                src={IMAGE_BASE_URL + movieDetails.poster_path}
+              />
+            </div>
+          </div>
+          <p>{movieDetails.overview}</p>
+        </section>
       </section>
       <Image
         loading="lazy"
-        className="h-screen object-cover  opacity-20 backdrop-shadow"
+        className=" h-screen  mobile-bg min-h-full object-cover  opacity-20 backdrop-shadow"
         width={600}
         height={1000}
         src={IMAGE_BASE_URL + movieDetails.backdrop_path}
